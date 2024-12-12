6f3cbc8f34cdf1607568ee88c1021905eb01f5f9
Nicolas Maurice <nicolas.maurice.valera@gmail.com>
2024-12-12 23:56:40 +0100

    feat(main.go): add main that generate provable inputs for latest block

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	ethrpc "github.com/kkrt-labs/kakarot-controller/pkg/ethereum/rpc/jsonrpc"
+	jsonrpchttp "github.com/kkrt-labs/kakarot-controller/pkg/jsonrpc/http"
+	"github.com/kkrt-labs/kakarot-controller/src/blocks"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	logrus.SetLevel(logrus.InfoLevel)
+	cfg := &blocks.Config{
+		RPC: &jsonrpchttp.Config{Address: os.Getenv("RPC_URL")},
+	}
+
+	svc := blocks.New(cfg)
+	err := svc.Generate(context.Background(), ethrpc.MustFromBlockNumArg("latest"))
+	if err != nil {
+		logrus.Fatalf("Failed to generate block inputs: %v", err)
+	}
+	logrus.Info("Blocks inputs generated")
+}
